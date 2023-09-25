de093960a7829904b44e5dc51add6fae580c6719
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-09-25 16:14:52 +0100

    start

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
-function concatArrays(array1, array2, string) {
-  return [...array1];
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
-
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
