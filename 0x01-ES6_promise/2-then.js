68c2ade1d2687d24c3a7dd642d4811add72a5ff1
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-09-26 22:48:03 +0100

    ES6 Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise((resolve,reject) => {
-    
-  })
-  .then(() => {})
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return ({ status: 200, body: 'Success' });
+    })
+    .catch((err) => {
+      console.error(err);
+      return (Error(''));
+    });
 }
