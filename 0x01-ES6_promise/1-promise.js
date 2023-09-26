ae506b6d3f160535c12bacaf4302c58488a1bd01
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-09-26 22:27:21 +0100

    ES6 Promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,3 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => (success ? resolve({ status: 200, body: 'Success' }) : reject(Error('The fake API is not working currently'))));
 }
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
