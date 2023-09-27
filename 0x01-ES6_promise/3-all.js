2cc9f43de0d9b1bf29390d908a808ae6dee56009
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-09-27 07:42:32 +0100

    ES6 Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([result1, result2]) => {
     console.log(result1.body, result2.firstName, result2.lastName);
-  });
+  })
+    .catch(() => console.log('Signup system offline'));
 }
