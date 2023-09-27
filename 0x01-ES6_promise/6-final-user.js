95b41a093ca1d82a1d8a37cc197daae0bff58a67
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-09-27 08:33:59 +0100

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,14 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, filename) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then(([result1, result2]) => ([
-      {
-        status: result1.status,
-        value: result1.status === 'fulfilled' ? result1.status : String(result1.reason),
-      },
-      {
-        status: result2.status,
-        value: result2.status === 'fulfilled' ? result2.status : String(result2.reason),
-      },
-    ]));
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
